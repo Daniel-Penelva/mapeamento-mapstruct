1d5d872a2e1cc5ef89c9a95772775317a0fa9f38
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-30 18:26:57 -0300

    Criando Projeto Mapeamento MapStruct

---
+++ src/main/java/com/daniel/mapeamentomapstruct/MapeamentoMapstructApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.mapeamentomapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentoMapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeamentoMapstructApplication.class, args);
+	}
+
+}
