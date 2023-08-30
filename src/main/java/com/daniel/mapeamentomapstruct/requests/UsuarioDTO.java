8c311f9e48db6ce3fce1f0a9a18180b9942b189b
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-30 18:53:54 -0300

    Criando UsuarioDTO

---
+++ src/main/java/com/daniel/mapeamentomapstruct/requests/UsuarioDTO.java
@@ -0,0 +1,11 @@
+package com.daniel.mapeamentomapstruct.requests;
+
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+    
+    private Long id;
+    private String name;
+    private String email;
+}
